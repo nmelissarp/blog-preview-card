fd305f2201573e2c9089d199f6a4620e532699ca
nmelissarp <nmelissarp077@gmail.com>
2025-01-10 11:58:12 -0600

    fix content's text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <p className={styles.date}>Published 21 Dec 2023</p>
           <h1>HTML & CSS foundations</h1>
           <p>
-            These languages are used the backbone of every website, defining
+            These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
         </section>
